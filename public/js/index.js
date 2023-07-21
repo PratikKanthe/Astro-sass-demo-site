5c057588a531b7c295eb5b54f4d856e0d38d9bf6
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-07-21 18:29:25 +0530

    done with ui

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,11 @@
 console.log("hi");
+alert('turn off your device')
 
 const hamburger = document.getElementById("hamburger");
 
 const navMenu = document.getElementById("nav-items");
+
+const dropdown = document.getElementById("dropdown");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -12,4 +15,13 @@ hamburger.addEventListener("click", () => {
 navMenu.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}));
+
+//dropdown
+dropdown.addEventListener("click", () => {
+    dropdown.classList.toggle("active");
+});
+
+navMenu.querySelectorAll("dropdown").forEach(n => n.addEventListener("click", () => {
+    dropdown.classList.remove("active");
 }));
